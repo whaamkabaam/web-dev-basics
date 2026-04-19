450ae213430ffed85748c97b527279263db6bb8d
whaamkabaam <hi@whaamkabaam.com>
2026-04-19 22:28:53 +0200

    add pretty urls

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+
+app.get('/tracker', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/tracker.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/about.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
