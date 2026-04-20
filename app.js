d07d39796da4e528a9a35355ec435261af6b0fc8
whaamkabaam <hi@whaamkabaam.com>
2026-04-20 21:08:34 +0200

    use ejs templates

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.get('/tracker', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/tracker.html'));
+  res.render('tracker');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/about.html'));
+  res.render('about');
 });
 
 app.listen(port, () => {
