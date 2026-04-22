ebba2a80addd2619f5bec1e43ec1b1c9f3bb255a
whaamkabaam <hi@whaamkabaam.com>
2026-04-22 23:11:05 +0200

    add block form

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const blocks = [];
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/tracker', (req, res) => {
-  res.render('tracker');
+  res.render('tracker', { blocks });
+});
+
+app.post('/tracker', (req, res) => {
+  const time = req.body.time;
+  const activity = req.body.activity;
+  const project = req.body.project;
+  blocks.push({ time, activity, project });
+  res.redirect('/tracker');
 });
 
 app.get('/about', (req, res) => {
