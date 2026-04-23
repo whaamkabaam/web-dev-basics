b1dbdb2a8cd82df58d8153c839b2eeb2649fb0d5
whaamkabaam <hi@whaamkabaam.com>
2026-04-23 20:34:51 +0200

    connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
