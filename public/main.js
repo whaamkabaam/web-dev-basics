0c97c5589ab418db21dd3289bf60bc6451f11118
whaamkabaam <hi@whaamkabaam.com>
2026-04-25 15:34:42 +0200

    confirm before delete

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll('.delete-btn');
 
 buttons.forEach((button) => {
   button.addEventListener('click', async (e) => {
+    if (!confirm('Delete this block?')) return;
     const id = e.currentTarget.dataset.id;
     await fetch('/tracker/' + id, { method: 'DELETE' });
   });
