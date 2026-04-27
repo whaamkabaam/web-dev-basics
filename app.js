6f24d34007ec6258374d0479f04ef54b4e2776b5
whaamkabaam <hi@whaamkabaam.com>
2026-04-27 12:50:55 +0200

    prep for deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Block = require('./models/block');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
